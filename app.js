96221610b67bdf8fc33bd116c4bed95cf38af925
Naakmsc <isaigutierrez125@gmail.com>
2022-02-11 15:05:23 -0600

    Arreglando el comit anterior

---
+++ app.js
@@ -0,0 +1,13 @@
+//ES5 - Variables
+var nombre = "Jai";
+console.log(nombre);
+nombre = "isai";
+console.log(nombre);
+
+//ES6+ - Variables
+const nombre6 = "oscar";
+let edad = 35;
+console.log(nombre6);
+console.log(edad);
+edad = 40;
+console.log(edad);
