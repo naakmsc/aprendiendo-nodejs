dd758e6925df4940ce87f5d4d27ac0e21cdee495
Naakmsc <isaigutierrez125@gmail.com>
2022-02-11 15:23:16 -0600

    Aprendiendo Funciones del tipo Callback

--- 01-es6+/app.js
+++ 01-es6+/app.js
@@ -13,18 +13,36 @@
 // console.log(edad);
 
 
-//Funciones Flechas
+// //Funciones Flechas
 
-//Funciones en ES5
-const years = [2000,2005,2008,2012];
+// //Funciones en ES5
+// const years = [2000,2005,2008,2012];
 
-var edad5 = years.map(function(el){
-    return 2019 - el;
-});
+// var edad5 = years.map(function(el){
+//     return 2019 - el;
+// });
 
-// console.log(edad5);
+// // console.log(edad5);
 
-//Funciones de ES6+ tipo Flecha
-let actual = 2019;
-let edad6 = years.map(el=>{return actual - el});
-console.log(edad6);
+// //Funciones de ES6+ tipo Flecha
+// let actual = 2019;
+// let edad6 = years.map(el=>{return actual - el});
+// console.log(edad6);
+
+
+//Funciones CallBack
+
+function mensaje(n1,n2,callback){
+    let resultado = n1 + n2;
+    console.log("Mensaje antes de la llamada Callback");
+    callback(resultado);
+}
+
+function saludo(res){
+    console.log(`Resultado ${res}`);
+}
+
+// mensaje(5,8,saludo);
+
+setTimeout(()=>console.log("Esto se va a ejecutar luego de 3 segundos")
+,3000);
