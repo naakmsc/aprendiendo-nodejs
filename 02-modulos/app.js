a60d2e41f7246aed24c5e5ee721648aba6e04f63
Naakmsc <isaigutierrez125@gmail.com>
2022-02-12 00:39:03 -0600

    Aprendiendo lo basico en el manejo de Eventos con Node

--- 02-modulos/app.js
+++ 02-modulos/app.js
@@ -20,12 +20,23 @@
 
 // console.log(`Memoria libre: ${memlibre} y memoria total: ${memtotal}`);
 
-//Modulo FIle System
-const fs = require("fs");
-const archivo = fs.readdirSync("./");
-console.log(archivo);
+// //Modulo FIle System
+// const fs = require("fs");
+// const archivo = fs.readdirSync("./");
+// console.log(archivo);
 
-fs.readdir("./", function(error,files){
-    if(error) console.log("Errorcito: ",error.message);
-    else console.log("Resultado: ",files);
+// fs.readdir("./", function(error,files){
+//     if(error) console.log("Errorcito: ",error.message);
+//     else console.log("Resultado: ",files);
+// });
+
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+//Registrar el Listener
+emitter.on("mensajeLoger",(args)=>{
+    console.log("Listener llamado...",args.id);
 });
+
+//Registrar el evento
+emitter.emit("mensajeLoger",{id:1,url:"http://wwww.google.com"});
