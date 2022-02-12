562252ba46d90b421c5462e4fecb62a9e0c1303c
Naakmsc <isaigutierrez125@gmail.com>
2022-02-12 00:06:13 -0600

    Aprendiendo un poco sobre el modulo PATH

--- 02-modulos/app.js
+++ 02-modulos/app.js
@@ -1,3 +1,8 @@
-let datos = require("./datos");
-
+const datos = require("./datos.js");
 datos.log("hello");
+
+const path = require("path");
+
+const objPath = path.parse(__filename);
+
+console.log(objPath.name);
--- 02-modulos/datos.js
+++ 02-modulos/datos.js
@@ -4,7 +4,6 @@ function datos(sms){
     console.log(sms);
 }
 
+
 module.exports.log = datos;
 module.exports.url = url;
-
-// console.log(module);
