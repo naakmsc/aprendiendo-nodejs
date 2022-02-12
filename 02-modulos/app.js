c99adc11f907d8697ea2af38c2f04f0b7488a6cd
Naakmsc <isaigutierrez125@gmail.com>
2022-02-12 00:34:18 -0600

    Aprendiendo el uso basico de File System de Node

--- 02-modulos/app.js
+++ 02-modulos/app.js
@@ -1,8 +1,31 @@
-const datos = require("./datos.js");
-datos.log("hello");
+// const datos = require("./datos.js");
+// datos.log("hello");
 
-const path = require("path");
 
-const objPath = path.parse(__filename);
 
-console.log(objPath.name);
+// //Modulo Path
+// const path = require("path");
+
+// const objPath = path.parse(__filename);
+
+// console.log(objPath.name);
+
+
+
+// //Modulo OS
+// const os =  require("os");
+
+// let memlibre = os.freemem();
+// let memtotal = os.totalmem();
+
+// console.log(`Memoria libre: ${memlibre} y memoria total: ${memtotal}`);
+
+//Modulo FIle System
+const fs = require("fs");
+const archivo = fs.readdirSync("./");
+console.log(archivo);
+
+fs.readdir("./", function(error,files){
+    if(error) console.log("Errorcito: ",error.message);
+    else console.log("Resultado: ",files);
+});
